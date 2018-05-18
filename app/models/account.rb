12fe1ab5fe352a7548f09c9126a47c5b67e93078
Andre Vidic <andre@dtcorp.com.au>
2018-05-19 09:48:12 +1000

    Account-Add debit & credit relation to Transaction

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,2 +1,4 @@
 class Account < ApplicationRecord
+  has_many :debit_trans, class_name: "Transaction", foreign_key: "debit_id"
+  has_many :credit_trans, class_name: "Transaction", foreign_key: "credit_id"
 end
