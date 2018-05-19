3abf3c5d957f3224a66e1f1a6d93cfa72be6bc0b
Michael Pope <map7777@gmail.com>
2018-05-19 11:02:54 +1000

    Trans-Load test data into transaction

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,4 +1,20 @@
 class Transaction < ApplicationRecord
   belongs_to :credit, class_name: "Account"
   belongs_to :debit, class_name: "Account"
+
+  def self.load
+    qif = Qif::Reader.new(open("#{Rails.root}/test/testdata.qif"))
+    debit = Account.where(code: 700).first
+    credit = Account.where(code: 999).first
+    
+    qif.each do |tran|
+      new_transaction = Transaction.new(date: tran.date,
+                                        description: tran.memo,
+                                        amount_cents: (tran.amount * 100),
+                                        debit: debit,
+                                        credit: credit)
+      new_transaction.save
+    end
+  end
 end
+
