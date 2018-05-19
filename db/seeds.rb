1de1c663f8f7ec837086d584edce786e26ab472e
Michael Pope <map7777@gmail.com>
2018-05-19 12:48:29 +1000

    DB SEED-Add filters to the seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,9 +8,21 @@
 
 require 'csv'
 
-CSV.parse(File.read("#{Rails.root}/db/#{CHART_OF_ACCOUNTS}")) do |account|
-  # Skip any lines beginning with # as these are comments.
-  unless account[0] =~ /^#.*/ or account[0].nil?
-    Account.create(code: account[0].strip, name: account[1].strip)
+# Accounts seed
+if Account.count == 0
+  CSV.parse(File.read("#{Rails.root}/db/#{CHART_OF_ACCOUNTS}")) do |account|
+    # Skip any lines beginning with # as these are comments.
+    unless account[0] =~ /^#.*/ or account[0].nil?
+      Account.create(code: account[0].strip, name: account[1].strip)
+    end
+  end
+end
+
+# Filter seed
+if Filter.count == 0
+  petrol = Account.find_by_name("Petrol")
+  ["BP", "CALTEX", "7-ELEVEN", "PETROL"].each do |filter_keyword|
+    petrol.filters.create(keyword: filter_keyword,
+                          account: petrol)
   end
 end
