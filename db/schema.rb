f6b85533eb9cc01a3c55d2d654c202f96f8c05ad
Michael Pope <map7777@gmail.com>
2018-05-19 18:02:25 +1000

    DB-Add phrase to filter

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_19_023601) do
+ActiveRecord::Schema.define(version: 2018_05_19_080202) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 2018_05_19_023601) do
     t.integer "account_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "phrase"
   end
 
   create_table "transactions", force: :cascade do |t|
