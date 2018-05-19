44d5581a7f8fc86f0ff6aafa0d8e5a2ad9ae1912
Andre Vidic <andre@dtcorp.com.au>
2018-05-19 18:03:06 +1000

    Stimulus-Tell webpack about stimulus & load it

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,1 +1,11 @@
+import { Application } from "stimulus";
+import { definitionsFromContext } from "stimulus/webpack-helpers";
+import Turbolinks from "turbolinks";
+import Rails from "rails-ujs";
 
+const application = Application.start();
+const context = require.context("../controllers", true, /\.js$/);
+application.load(definitionsFromContext(context));
+
+Rails.start();
+Turbolinks.start();
