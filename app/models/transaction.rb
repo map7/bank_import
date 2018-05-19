2fe3d2636b836cfacabc799a6074cc7def729b93
Michael Pope <map7777@gmail.com>
2018-05-19 11:24:59 +1000

    Tran-Add extra stuff to the load

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -2,16 +2,24 @@ class Transaction < ApplicationRecord
   belongs_to :credit, class_name: "Account"
   belongs_to :debit, class_name: "Account"
 
-  def self.load
+  def self.load(file="#{Rails.root}/test/testdata.qif")
     # Clear the db
     Transaction.delete_all
 
     # Load the test data
-    qif = Qif::Reader.new(open("#{Rails.root}/test/testdata.qif"))
-    debit = Account.where(code: 700).first
-    credit = Account.where(code: 999).first
-    
+    qif = Qif::Reader.new(open(file))
+
     qif.each do |tran|
+      # Determine if it's a debit or credit
+      if tran.category == "DEBIT"
+        debit = Account.where(code: 700).first
+        credit = Account.where(code: 999).first
+      else        
+        credit = Account.where(code: 700).first
+        debit = Account.where(code: 999).first
+      end
+
+      # Create transaction
       new_transaction = Transaction.new(date: tran.date,
                                         description: tran.memo,
                                         amount_cents: (tran.amount * 100),
