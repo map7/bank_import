d8471ea01929a0c5a73ef0f27dab3e2227d9b27f
Andre Vidic <andre@dtcorp.com.au>
2018-05-20 17:52:48 +1000

    Transaction-Add destroy_many route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'transactions#new'
-  resources :transactions
-  resources :account_summary  
+  resources :transactions do
+    collection do
+      delete 'destroy_many', to: 'transactions#destroy_many'
+    end
+  end
+  resources :account_summary
+  
 end
