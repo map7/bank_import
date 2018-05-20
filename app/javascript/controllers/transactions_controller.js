78aadb0948f8f40cbe9feae5d717dbb6622576d7
Andre Vidic <andre@dtcorp.com.au>
2018-05-20 16:29:03 +1000

    Chart-Tell the controller where to get the chart
    Also on page load draw the chart

--- app/javascript/controllers/transactions_controller.js
+++ app/javascript/controllers/transactions_controller.js
@@ -1,6 +1,13 @@
 import { Controller } from "stimulus"
+import Chartkick from "chartkick"
+import Chart from "chart.js"
 
 export default class extends Controller {
+  initialize() {
+    
+    new Chartkick.PieChart("chart-1", [["Blueberry", 44], ["Strawberry", 23]])    
+  }
+
   file() {
     console.log(this.targets.find("file_paths").value)
   }
