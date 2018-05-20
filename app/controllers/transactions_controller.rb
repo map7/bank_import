ec0e239d8b3f3d436cc0f11449b848ae5d04183f
Andre Vidic <andre@dtcorp.com.au>
2018-05-20 13:40:55 +1000

    Transaction-Extract file path & send to transaction load method

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -4,7 +4,10 @@ class TransactionsController < ApplicationController
   end
 
   def create
-    binding.pry
+    file_path = transaction_params[:file_paths].tempfile.path
+    full_path = Rails.root.join(file_path).to_s
+    # Send file paths to load transaction method
+    Transaction.load(full_path)
   end
 
   def transaction_params
