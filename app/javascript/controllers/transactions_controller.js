82fcb0ffa56dadb50cda9783dc2bafeadc218acf
Andre Vidic <andre@dtcorp.com.au>
2018-05-20 19:23:39 +1000

    Transaction-Remove chart from js controller

--- app/javascript/controllers/transactions_controller.js
+++ app/javascript/controllers/transactions_controller.js
@@ -1,20 +1,7 @@
 import { Controller } from "stimulus"
-import Chartkick from "chartkick"
-import Chart from "chart.js"
 import Rails from "rails-ujs"
 
 export default class extends Controller {
-  initialize() {
-    fetch(this.data.get("url"))
-      .then(response =>
-            response.text())
-      .then(expenses => {
-        new Chartkick.PieChart("chart-1", JSON.parse(expenses))
-      })
-
-    
-  }
-
   destroy() {
     this.railsDelete("transactions/destroy_many");
   }
