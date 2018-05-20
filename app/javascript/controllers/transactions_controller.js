4f66239afcfa245be4cfd524d416d9bcaefc97af
Andre Vidic <andre@dtcorp.com.au>
2018-05-20 17:04:59 +1000

    Transaction-Display account_summary JSON in console

--- app/javascript/controllers/transactions_controller.js
+++ app/javascript/controllers/transactions_controller.js
@@ -4,8 +4,17 @@ import Chart from "chart.js"
 
 export default class extends Controller {
   initialize() {
-    
-    new Chartkick.PieChart("chart-1", [["Blueberry", 44], ["Strawberry", 23]])    
+    // new Chartkick.PieChart("chart-1", [["Blueberry", 44], ["Strawberry", 23]])
+    //new Chartkick.PieChart("chart-1", [["Transport", -16, 0], ["Car Expenses", -999, 0], ["Entertainment", -45, 0], ["Technology", -253, 0], ["Bank Fees", -86, 0], ["Insurance", -700, 100], ["paypal", -699, 0], ["Holiday", -710, 0], ["Alcohol", -166, 0], ["Pub", -338, 0], ["Pharmacy", -38, 0], ["Food", -1349, 0], ["Water", -63, 0], ["Internet", -80, 0], ["Electricity", -754, 0], ["Health", -305, 0]])
+
+    fetch(this.data.get("url"))
+      .then(response =>
+            response.text())
+      .then(html => {
+        console.log(html)
+      })
+
+    //new Chartkick.PieChart("chart-1", expenses)    
   }
 
   file() {
