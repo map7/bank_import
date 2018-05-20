e0cbacd2e89d0f7eff9b7212ababfe2dc7aadbca
Andre Vidic <andre@dtcorp.com.au>
2018-05-20 12:24:57 +1000

    Transaction-Sit on rails side and pickup front end submit

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -2,4 +2,12 @@ class TransactionsController < ApplicationController
   def new
     @transaction = Transaction.all
   end
+
+  def create
+    binding.pry
+  end
+
+  def transaction_params
+    params.require(:transaction).permit(:file_paths, :completed, :completed_filter)
+  end
 end
