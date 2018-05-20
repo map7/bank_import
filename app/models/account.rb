ab2442e3dd7fb1132907cc7ad122f0f8b79b8597
Michael Pope <map7777@gmail.com>
2018-05-20 12:11:50 +1000

    Account-Add a summary

--- app/models/account.rb
+++ app/models/account.rb
@@ -3,4 +3,17 @@ class Account < ApplicationRecord
   has_many :credit_trans, class_name: "Transaction", foreign_key: "credit_id"
   has_many :filters
 
+  def self.summary
+    results = []
+    Account.all.each do |acc|
+      expenses = acc.credit_trans.sum(:amount_cents) / 100
+      income = acc.debit_trans.sum(:amount_cents) / 100
+
+      if expenses < 0 or income > 0
+        results << {name: acc.name, expenses: expenses, income: income}
+      end
+    end
+    results
+  end
+  
 end
