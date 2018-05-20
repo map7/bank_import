6c3457b38870afc28bbafe5f03415335c32754be
Andre Vidic <andre@dtcorp.com.au>
2018-05-20 16:28:11 +1000

    Chart-Tell webpack about the charts

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,6 +2,8 @@ import { Application } from "stimulus";
 import { definitionsFromContext } from "stimulus/webpack-helpers";
 import Turbolinks from "turbolinks";
 import Rails from "rails-ujs";
+import Chartkick from "chartkick";
+import Chart from "chart.js";
 
 const application = Application.start();
 const context = require.context("../controllers", true, /\.js$/);
@@ -9,3 +11,4 @@ application.load(definitionsFromContext(context));
 
 Rails.start();
 Turbolinks.start();
+Chartkick.addAdapter(Chart);
