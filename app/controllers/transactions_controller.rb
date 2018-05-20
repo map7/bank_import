0d3904e2f2cb6953075b8d6fb6b3e2db13086664
Andre Vidic <andre@dtcorp.com.au>
2018-05-20 19:22:28 +1000

    Transaction-Redirect to account aummary to show chart

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -8,6 +8,7 @@ class TransactionsController < ApplicationController
     full_path = Rails.root.join(file_path).to_s
     # Send file paths to load transaction method
     Transaction.load(full_path)
+    redirect_to new_account_summary_path
   end
 
   def destroy_many
